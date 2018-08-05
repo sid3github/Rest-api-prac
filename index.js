34a06d785eadbdc7a707f52d4cdebe271ebfdc36
sid3github <siddharthpadwal3@gmail.com>
2018-08-05 17:50:49 +0530

    updated validations

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const Emp = require('./models/employees');
+const {check,validationResult} = require('express-validator/check');
 
 //set up express app
 const server = express();
@@ -16,15 +17,34 @@ server.use(bodyParser.json());
 
 //initialize routes
 server.get('/employees',function (req,res,next) {
-    res.send({type: 'GET'});
+    Emp.find(req.body).then(function (emp){
+    res.json(emp);
+});
 });
 //adding a new employee to db
-server.post('/employees',function (req,res,next) {
+//the .create method will both create and save the data received from the client and then again will be forwarded to client itself.
+server.post('/employees',[
+    check('name').isString(),
+    check('age').isNumeric(),
+    check('gender').isString()
+],function (req,res,next) {
+    const errors = validationResult(req);
+    if(!errors.isEmpty()){
+         return res.json({error: errors.array()});
+    }
     Emp.create(req.body).then(function (emp) {
         res.send(emp);
-    }).catch(next);
+    });
 });
+//  server.post('/employees,function(req,res,next){
+//  Emp.create(req.body).then(function(emp){
+//  res.send(emp)
+//  }).catch(next)
+// })
+
+
 //updating a employee to db
+//In put method we get a specific id selected by user to which the data is updated,findById will find the Id and with the help of req.body it will update the data,then send updated data to client. 
 server.put('/employees/:id',function (req,res,next) {
     Emp.findByIdAndUpdate({_id:req.params.id},req.body).then(function(emp){
         res.send(emp);
@@ -38,9 +58,9 @@ server.delete('/employees/:id',function (req,res,next) {
 });
 
 //Error handling middleware.
-server.use(function(err,req,res,next){
-    res.send({error: err.message});
-});
+// server.use(function(err,req,res,next){
+//     res.send({error: err.message});
+// });
 
 //listen for a request
 server.listen(2000, function () {
--- models/employees.js
+++ models/employees.js
@@ -1,20 +1,24 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
-//create schema and models
+//create schema
 const EmpSchema = new Schema({
    name: {
        type: String,
-       required: [true,'Name is required']
+       required: [true,'Name is required'],
+       pattern:
    },
     age: {
-       type: Number
+       type: Number,
+       required: [true,'Age is required']
     },
     gender: {
-       type: String
+       type: String,
+       required: [true,'Gender is required']
     }
 });
 
+//Creating mongoose.model(model name,schema name,collection name)
 const Emp = mongoose.model('emp',EmpSchema,'emmp');
 
 module.exports = Emp;
